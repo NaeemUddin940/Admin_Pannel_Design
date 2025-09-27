215df79dae0781ff4275dcc4612b7839f03a61f5
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-09-27 22:37:16 +0600

    Step - 02, Public Layout Setup Done

--- Admin_Pannel_Design_01/src/layouts/PublicLayouts.tsx
+++ Admin_Pannel_Design_01/src/layouts/PublicLayouts.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import Header from "@/components/Header/Header";
+import { Outlet } from "react-router";
+import Footer from "@/components/Footer/Footer";
 
 export default function PublicLayouts() {
   return (
-    <div>PublicLayouts</div>
-  )
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
