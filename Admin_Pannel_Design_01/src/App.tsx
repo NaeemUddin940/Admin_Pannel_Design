740ced17a9993d2027a418d1688e6d5fdb00db8c
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-09-27 22:43:47 +0600

    Step - 05, Finally Fully Route Setup Done

--- Admin_Pannel_Design_01/src/App.tsx
+++ Admin_Pannel_Design_01/src/App.tsx
@@ -1,14 +1,40 @@
-import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { Route, Routes } from "react-router";
+import PublicLayouts from "./layouts/PublicLayouts";
+import AdminRoute from "./admin/Routes/AdminRoute";
+import AdminLayouts from "./layouts/AdminLayouts";
+import NotFound from "./pages/NotFound";
+import Dashboard from "./admin/pages/Dashboard";
+import Users from "./admin/pages/Users";
+import Settings from "./admin/pages/Settings";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Routes>
+    <Routes>
+      {/* Public pages with header/footer */}
+      <Route element={<PublicLayouts />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+      </Route>
+
+      {/* Admin panel (completely isolated layout) */}
+      <Route
+        path="/admin"
+        element={
+          <AdminRoute>
+            <AdminLayouts />
+          </AdminRoute>
+        }>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+
+      {/* Catch-all */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
+
+export default App;
