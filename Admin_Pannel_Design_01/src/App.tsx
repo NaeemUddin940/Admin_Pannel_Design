54d7b0da7defc1b85f7a5d756f24042c604167e7
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-09-27 22:33:43 +0600

    Step - 01, Basic Route Setup Done

--- Admin_Pannel_Design_01/src/App.tsx
+++ Admin_Pannel_Design_01/src/App.tsx
@@ -1,3 +1,14 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 export default function App() {
-  return <div className="bg-red-500">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
