942646790792bc3777a078cf4ede553e5bceea52
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-09-27 22:40:06 +0600

    Step - 04, Admin Route Protection Done

---
+++ Admin_Pannel_Design_01/src/admin/Routes/AdminRoute.tsx
@@ -0,0 +1,14 @@
+// src/Admin/routes/AdminRoute.tsx
+import { Navigate } from "react-router";
+
+type AdminRouteProps = { children: React.ReactNode };
+
+const isAdmin = () => {
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  return user?.role === "admin";
+};
+
+export default function AdminRoute({ children }: AdminRouteProps) {
+  if (!isAdmin()) return <Navigate to="/login" replace />; // redirect non-admins
+  return <>{children}</>;
+}
--- Admin_Pannel_Design_01/src/admin/Routes/AdminRoutes.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AdminRoutes() {
-  return (
-    <div>AdminRoutes</div>
-  )
-}
